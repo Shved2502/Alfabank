7b3efe75d0ba51161981411a5f65034416a352ef
Vadim Shvedov <vadim_shvedov1@epam.com>
2022-06-02 16:22:30 +0300

    Initial commit

---
+++ src/main/java/com/example/alfabank/AlfabankApplication.java
@@ -0,0 +1,13 @@
+package com.example.alfabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfabankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlfabankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alfabank/AlfabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alfabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfabankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
